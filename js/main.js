54741905832c57e7e7925f054d656c26cef3a0f7
Alex12012000 <arcex.29@gmail.com>
2022-06-27 15:22:24 +0200

    Milestone 1

--- js/main.js
+++ js/main.js
@@ -1,4 +1,24 @@
 
 var app = new Vue ({
     el: '#root',
+    data: {
+        todos: [
+        {
+            text: 'Fare la spesa',
+            done: false
+        },
+        {
+            text: 'Chiamare nonna',
+            done: false
+        },
+        {
+            text: 'Pulire casa',
+            done: false
+        },
+        {
+            text: 'Giocare a calcetto',
+            done: true
+        },
+    ],
+    }
 })
