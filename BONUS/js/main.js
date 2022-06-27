bd31321e1762407fe5166da0ec97590a4ec86368
Alex12012000 <arcex.29@gmail.com>
2022-06-27 16:05:37 +0200

    Bonus 1

---
+++ BONUS/js/main.js
@@ -0,0 +1,36 @@
+
+var app = new Vue ({
+    el: '#root',
+    data: {
+        userInput: '',
+        newInput: null,
+        todos: [
+        {
+            text: 'Fare la spesa',
+            done: false
+        },
+        {
+            text: 'Chiamare nonna',
+            done: false
+        },
+        {
+            text: 'Pulire casa',
+            done: false
+        },
+        {
+            text: 'Giocare a calcetto',
+            done: true
+        },
+    ],
+    },
+    methods: {
+        removeOnClick(elementIndex) {
+            this.todos.splice(elementIndex, 1);
+        },
+        addOnClick() {
+            this.newInput = {text: this.userInput, done: false} 
+            this.todos.push(this.newInput);
+            this.userInput = ''
+        }
+    },
+})
